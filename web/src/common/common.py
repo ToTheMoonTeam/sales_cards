22e6b1a32a847de244c5992f2309e5edb42ae46a
Vasiliy566 <vasyaisaev31@gmail.com>
2021-04-07 00:41:56 +0300

    addexcele export

--- web/src/common/common.py
+++ web/src/common/common.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 import logging
+import xlsxwriter
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
@@ -28,3 +29,27 @@ def wrong_argument_provided(argument, reason):
     resp.status_code = 422
     return resp
 
+
+def generate_excele(users):
+    # Create a workbook and add a worksheet.
+    workbook = xlsxwriter.Workbook('AllUsers.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    datetime_format = workbook.add_format({'num_format': 'dd/mm/yy'})
+    header = ["id",
+              "name",
+              "phone_number",
+              "birthday",
+              "work_quality",
+              "shipping_quality"]
+    for item in header:
+        worksheet.write(0, header.index(item), item)
+    row = 1
+    for item in users:
+        for col in range(len(item)):
+            if col == 3:
+                worksheet.write(row, col, item[col], datetime_format)
+            else:
+                worksheet.write(row, col, item[col])
+        row += 1
+    workbook.close()
